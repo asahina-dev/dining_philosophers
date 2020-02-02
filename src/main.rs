a2c90a589cb8ffd8c9a540f78564c16e758b1518
asahina-dev <0.0.nn.ai@gmail.com>
2020-02-02 14:24:56 +0900

    feat:哲学者たちが一瞬で食事を終えるところまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// 構造体
+struct Philosopher {
+    name: String,
+}
+
+// 指定した構造体に対する定義
+impl Philosopher {
+    // Philosopherインスタンスを返す
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Vec<T>: 可変長の配列型
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+    for p in &philosophers {
+        p.eat();
+    }
 }
